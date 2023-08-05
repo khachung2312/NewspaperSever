f4c0e0e36d1aa0e66fc328ff920c8b59d5f3b2c2
linhmanh <linhmanhng@gmail.com>
2023-08-06 02:03:01 +0700

    up account.js

---
+++ account.js
@@ -0,0 +1,127 @@
+const express =  require('express');
+const app = express();
+const port = 3000;
+const db = require('./connect');
+const nodemailer = require('nodemailer');
+var bodyParser = require('body-parser');
+
+var md5 = require('md5');
+
+app.use(bodyParser.json())
+
+app.post('/accounts/login', (req, res) => {
+    
+    const { email, password_of_user } = req.body;
+  
+    db.query('SELECT * FROM Accounts WHERE email = ? AND password_of_user = ?', [email, md5(password_of_user)], (error, results) => {
+      if (error) {
+        console.error('Lỗi truy vấn:', error);
+        res.status(500).send('Lỗi server');
+      } else if (results.length === 0) {
+        res.status(401).send('Tên đăng nhập hoặc mật khẩu không chính xác');
+      } else {
+        res.json(results[0]);
+        console.log('Đăng nhập thành công');
+      }
+    });
+});
+
+app.post('/accounts/register', (req, res) => {
+    const { email, password_of_user } = req.body;
+
+    const account = { email, password_of_user };
+
+    db.query('INSERT INTO Accounts SET ?', account, (error, results) => {
+        if (error) {
+            console.error('Lỗi truy vấn:', error);
+            res.status(500).send('Lỗi server');
+            console.log(email);
+        } else {
+            const insertedUserId = results.insertId;
+            res.json({ IDUser: insertedUserId });
+            console.log("Đăng kí tài khoản thành công");
+        }
+    });
+});
+
+// app.put('/accounts/changePassword', (req, res) => {
+
+//     const { email, oldPassword, newPassword } = req.body;
+//     console.log(req.body)
+//     console.log(md5(oldPassword))
+
+//     db.query('SELECT * FROM Accounts WHERE email = ? AND password_of_user = ?', [email, md5(oldPassword)], (error, results) => {
+//         if (error) {
+//             console.error('Lỗi truy vấn:', error);
+//             res.status(500).send('Lỗi server');
+//         } else if (results.length === 0) {
+//             res.status(401).send('Mật khẩu cũ không chính xác');
+//         } else {
+//             db.query('UPDATE Accounts SET password_of_user = ? WHERE email = ?', [md5(newPassword), email], (error, updateResults) => {
+//                 if (error) {
+//                     console.error('Lỗi truy vấn:', error);
+//                     res.status(500).send('Lỗi server');
+//                 } else {
+//                     res.json({ message: 'Mật khẩu đã được cập nhật thành công' });
+//                     console.log('Cập nhật mật khẩu thành công');
+//                 }
+//             });
+//         }
+//     });
+// });
+
+const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'linhmanhng@gmail.com',
+      pass: 'aB011602'
+    }
+});
+
+function generateRandomPassword() {
+    const length = 10;
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let newPassword = '';
+    for (let i = 0; i < length; i++) {
+      newPassword += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return newPassword;
+  }
+
+app.post('/accounts/forgotPassword', (req, res) => {
+    const { email } = req.body;
+  
+    const newPassword = generateRandomPassword();
+  
+    const mailOptions = {
+      from: 'linhmanhng@gmail.com',
+      to: email,
+      subject: 'Thông báo mật khẩu mới',
+      text: `Mật khẩu mới của bạn là: ${newPassword}`
+    };
+  
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Lỗi khi gửi email:', error);
+        res.status(500).send('Lỗi server');
+      } else {
+        res.json({ message: 'Email đã được gửi thành công, vui lòng kiểm tra hộp thư đến của bạn.' });
+      }
+    });
+});
+
+
+app.get('/accounts', (req, res) => {
+    db.query('SELECT * FROM Accounts', (error, results) => {
+        if (error) {
+            console.log('Lỗi truy vấn', error);
+            res.status(500).send('Lỗi server')
+        } else {
+            res.json(results);
+        }
+    });    
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
